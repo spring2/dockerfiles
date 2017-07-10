7132f7ec4ca2a312e9cc1eacbd24ba8d36b6555b
Cort A. Schaefer <cort@eschaefer.net>
2017-07-10 08:21:07 -0600

    add latest-nanoserver tag

--- nginx/build.bat
+++ nginx/build.bat
@@ -1,5 +1,5 @@
-docker build -t spring2/nginx -f Dockerfile .
+docker build -t spring2/nginx:latest -f Dockerfile .
 docker tag spring2/nginx:latest spring2/nginx:1.13.0
 
-docker build -t spring2/nginx -f Dockerfile.nanoserver .
-docker tag spring2/nginx:latest spring2/nginx:1.11.13-nanoserver
+docker build -t spring2/nginx:latest-nanoserver -f Dockerfile.nanoserver .
+docker tag spring2/nginx:latest-nanoserver spring2/nginx:1.11.13-nanoserver
--- nginx/push.bat
+++ nginx/push.bat
@@ -2,3 +2,4 @@ docker push spring2/nginx:1.13.0
 docker push spring2/nginx:latest
 
 docker push spring2/nginx:1.11.13-nanoserver
+docker push spring2/nginx:latest-nanoserver
