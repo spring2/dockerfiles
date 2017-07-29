e9cd57164d06112c0f47623fda48833b358d17f0
Cort A. Schaefer <cort@eschaefer.net>
2017-07-29 13:47:21 -0600

    setup build scripts like other builds

---
+++ rabbitmq/build.bat
@@ -0,0 +1,2 @@
+docker build -t spring2/rabbitmq:latest -f Dockerfile .
+docker tag spring2/rabbitmq:latest spring2/rabbitmq:3.6.10
---
+++ rabbitmq/push.bat
@@ -0,0 +1,2 @@
+docker push spring2/rabbitmq:3.6.10
+docker push spring2/rabbitmq:latest
