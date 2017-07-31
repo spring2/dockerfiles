ed879be9598614390ccb9ae6d44eb9cd44670c5f
Cort A. Schaefer <cort@eschaefer.net>
2017-07-31 08:02:03 -0600

    remove old style rabbit build files

--- rabbitmq/build.bat
+++
@@ -1,2 +0,0 @@
-docker build -t spring2/rabbitmq:latest -f Dockerfile .
-docker tag spring2/rabbitmq:latest spring2/rabbitmq:3.6.10
--- rabbitmq/push.bat
+++
@@ -1,2 +0,0 @@
-docker push spring2/rabbitmq:3.6.10
-docker push spring2/rabbitmq:latest
